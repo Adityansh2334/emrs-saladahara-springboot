a535f6a91fe5fae21b11048e142cd27ec83b6ee2
Adityansh2334 <adityabehera23@gmail.com>
2025-05-03 08:07:47 +0530

    Initial Push- emrs-website-backend-spring boot

---
+++ src/main/java/org/labs/jkcloud/emrs/EmrsSaladahraNlgBackApplication.java
@@ -0,0 +1,13 @@
+package org.labs.jkcloud.emrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrsSaladahraNlgBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmrsSaladahraNlgBackApplication.class, args);
+    }
+
+}
